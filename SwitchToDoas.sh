8402b67fbec678c19b3959ba0574543ea213fb17
quest99812 <quest99812@users.noreply.github.com>
2026-07-12 14:57:24 +0200

    simplified writing config to the file, instead of changing directories it stays on the default one and writes what it needs

--- SwitchToDoas.sh
+++ SwitchToDoas.sh
@@ -1,8 +1,5 @@
 sudo pacman -S opendoas #this is for installing an opendoas package
-cd /etc #changes directory
-sudo touch 'doas.conf' #makes a new file
-echo "permit persist setenv {PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin} :wheel" | sudo tee doas.conf  # doas provides a persist feature. After the user successfully authenticates, they will not be prompted for a password again for 5 minutes.
-cd #reverts to the default directory
+echo "permit persist setenv {PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin} :wheel" | sudo tee /etc/doas.conf  # doas provides a persist feature. After the user successfully authenticates, they will not be prompted for a password again for 5 minutes. The command itself is for writing the config for doas file located in /etc/ directory
 doas pacman -Rdd sudo #removes sudo while ignoring broken dependencies
 ln -s $(which doas) /usr/bin/sudo #any command using "sudo" will be using "doas" instead. For an example, instead of "sudo pacman -Syyu" the system will understand as "doas pacman -Syyu"
 
